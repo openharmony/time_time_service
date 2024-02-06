70500b4255d47dd1f49f3bb246996ef888541fa9
Jeam.wang <jeam.wangwei@huawei.com>
2024-02-06 01:59:42 +0000

    update services/time/src/sntp_client.cpp.
    Signed-off-by: Jeam.wang <jeam.wangwei@huawei.com>

--- services/time/src/sntp_client.cpp
+++ services/time/src/sntp_client.cpp
@@ -304,10 +304,10 @@ void SNTPClient::ReceivedMessage(char *buffer)
     int64_t _receiveServer = ConvertNtpToStamp(_sntpMsg._receiveTimestamp);
     int64_t _transmitServer = ConvertNtpToStamp(_sntpMsg._transmitTimestamp);
     int64_t _receiveClient = ConvertNtpToStamp(_ntpTs);
-    TIME_HILOGI(TIME_MODULE_SERVICE, "_originClient:%{public}s _receiveServer:%{public}s _transmitServer:%{public}s "
+    TIME_HILOGI(TIME_MODULE_SERVICE, "_originClient:%{public}s, _receiveServer:%{public}s, _transmitServer:%{public}s,"
                 "_receiveClient:%{public}s", std::to_string(_originClient).c_str(),
                 std::to_string(_receiveServer).c_str(), std::to_string(_transmitServer).c_str(),
-                std::to_string(_receiveClient)).c_str();
+                std::to_string(_receiveClient).c_str());
     int64_t _clockOffset = (((_receiveServer - _originClient) + (_transmitServer - _receiveClient)) / INDEX_TWO);
     int64_t _roundTripDelay = (_receiveClient - _originClient) - (_transmitServer - _receiveServer);
     mRoundTripTime = _roundTripDelay;
