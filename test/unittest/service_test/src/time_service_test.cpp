154c6d274860081ac618b5ac29b679438e2aacfe
gaozhichao <gaozhichao5@huawei.com>
2024-10-11 18:37:40 +0800

    [fix] 修改代理状态下定时器恢复机制
    Signed-off-by: gaozhichao <gaozhichao5@huawei.com>

--- test/unittest/service_test/src/time_service_test.cpp
+++ test/unittest/service_test/src/time_service_test.cpp
@@ -1130,7 +1130,7 @@ HWTEST_F(TimeServiceTest, TimerManager004, TestSize.Level0)
         TimerProxy::GetInstance().proxyUids_.insert(std::make_pair(UID, timePointMap));
     }
     auto res = TimerManager::GetInstance()->StartTimer(TIMER_ID, 0);
-    EXPECT_EQ(res, E_TIME_DEAL_FAILED);
+    EXPECT_EQ(res, E_TIME_OK);
 
     {
         std::lock_guard<std::mutex> lock(TimerProxy::GetInstance().proxyMutex_);
@@ -1147,7 +1147,7 @@ HWTEST_F(TimeServiceTest, TimerManager004, TestSize.Level0)
         map.insert(std::make_pair(UID, timePointMap));
     }
     res = TimerManager::GetInstance()->StartTimer(TIMER_ID, 0);
-    EXPECT_EQ(res, E_TIME_DEAL_FAILED);
+    EXPECT_EQ(res, E_TIME_OK);
 
     {
         std::lock_guard<std::mutex> lock(TimerProxy::GetInstance().proxyPidMutex_);
