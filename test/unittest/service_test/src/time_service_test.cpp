de2a757e3deb7faa459f446ba2a30a942f42f89b
SurName <472256532@qq.com>
2025-02-18 15:32:54 +0800

    Delete Tc Require
    Signed-off-by: xujunyang <xujunyang1@h-partners.com>

--- test/unittest/service_test/src/time_service_test.cpp
+++ test/unittest/service_test/src/time_service_test.cpp
@@ -305,7 +305,7 @@ HWTEST_F(TimeServiceTest, PidProxyTimer003, TestSize.Level0)
 * @tc.name: PidProxyTimer004.
 * @tc.desc: proxy timer.
 * @tc.type: FUNC
-* @tc.require: SR000H0GQ6 AR000H2VTQ
+* @tc.require:
 */
 HWTEST_F(TimeServiceTest, PidProxyTimer004, TestSize.Level0)
 {
@@ -319,7 +319,7 @@ HWTEST_F(TimeServiceTest, PidProxyTimer004, TestSize.Level0)
 * @tc.name: AdjustTimer001.
 * @tc.desc: adjust timer.
 * @tc.type: FUNC
-* @tc.require: AR20240306656104
+* @tc.require:
 */
 HWTEST_F(TimeServiceTest, AdjustTimer001, TestSize.Level0)
 {
@@ -333,7 +333,7 @@ HWTEST_F(TimeServiceTest, AdjustTimer001, TestSize.Level0)
 * @tc.name: AdjustTimer002.
 * @tc.desc: exemption timer.
 * @tc.type: FUNC
-* @tc.require: AR20240306656104
+* @tc.require:
 */
 HWTEST_F(TimeServiceTest, AdjustTimer002, TestSize.Level0)
 {
