cf16f81206bd4d7a3fa443c733e67ce7c191a37d
林川 <390475943@qq.com>
2025-06-09 12:20:00 +0000

    update test/unittest/service_test/src/time_service_timer_test.cpp.
    Signed-off-by: 林川 <390475943@qq.com>

--- test/unittest/service_test/src/time_service_timer_test.cpp
+++ test/unittest/service_test/src/time_service_timer_test.cpp
@@ -1959,6 +1959,28 @@ HWTEST_F(TimeServiceTimerTest, TimerInfo009, TestSize.Level0)
 }
 
 /**
+* @tc.name: TimerInfo010.
+* @tc.desc: test CheckStatusBeforeAdjust.
+* @tc.type: FUNC
+*/
+HWTEST_F(TimeServiceTimerTest, TimerInfo010, TestSize.Level0)
+{
+    auto zero = milliseconds(0);
+    std::chrono::steady_clock::time_point empty (zero);
+    auto timerInfo = TimerInfo("", 0, ITimerManager::ELAPSED_REALTIME, zero, empty, zero, empty, zero, nullptr,
+                               nullptr, 0, false, 0, 0, "");
+    EXPECT_EQ(timerInfo.state, TimerInfo::TimerState::INIT);
+    auto timePoint = std::chrono::steady_clock::now();
+    timerInfo.AdjustTimer(timePoint, 1, 0);
+    EXPECT_TRUE(timerInfo.CheckStatusBeforeAdjust());
+    EXPECT_EQ(timerInfo.state, TimerInfo::TimerState::ADJUST);
+
+    timerInfo.ProxyTimer(empty, milliseconds(3000));
+    EXPECT_EQ(timerInfo.state, TimerInfo::TimerState::PROXY);
+    EXPECT_TRUE(timerInfo.CheckStatusBeforeAdjust());
+}
+
+/**
 * @tc.name: ResetAllProxy001.
 * @tc.desc: test RefreshNetworkTimeByTimer.
 * @tc.type: FUNC
